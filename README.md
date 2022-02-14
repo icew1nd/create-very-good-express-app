caf48cfc3650179af05c62b0ddebe4da3f216030
Thomas Kjær-Rasmussen <blabla928@hotmail.com>
2022-02-15 00:36:09 +0100

    Added awesome rainbowy things

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demo
+
+This starter kit is already deployed to npm and can be tried with `npx create-very-good-express-app project-name`
--- create.js
+++ create.js
@@ -1,27 +1,33 @@
 #! /usr/bin/env node
 
-var fs = require("fs-extra");
-var installFolder = process.argv.slice(2)[0];
-var path = require("path");
+const fs = require("fs-extra");
+const installFolder = process.argv.slice(2)[0];
+const path = require("path");
+const gradient = require("gradient-string");
+
 try {
-  fs.copySync(path.join(__dirname, "./template"), "./" + installFolder);
-  console.log("Copied base files");
-  const rawdata = fs.readFileSync("./" + installFolder + "/package.json");
-  const pkg = JSON.parse(rawdata);
+  if (installFolder) {
+    fs.copySync(path.join(__dirname, "./template"), "./" + installFolder);
+    console.log("Copied base files");
+    const rawdata = fs.readFileSync("./" + installFolder + "/package.json");
+    const pkg = JSON.parse(rawdata);
 
-  pkg.name = installFolder;
+    pkg.name = installFolder;
 
-  const pkgStr = JSON.stringify(pkg, null, 2);
-  fs.writeFileSync("./" + installFolder + "/package.json", pkgStr);
+    const pkgStr = JSON.stringify(pkg, null, 2);
+    fs.writeFileSync("./" + installFolder + "/package.json", pkgStr);
 
-  console.log("Updated package.json");
+    console.log("Updated package.json");
 
-  console.log("\n-----------\n");
-  console.log("Your project is ready, get started by \n");
+    console.log("\n-----------\n");
+    console.log(gradient.rainbow("Your Express API is ready! \n"));
 
-  console.log("cd " + installFolder + "\n");
-  console.log("npm i, to install dependencies");
-  console.log("npm dev, to run");
+    console.log("cd " + installFolder + "\n");
+    console.log("npm i, to install dependencies");
+    console.log("npm dev, to run");
+  } else {
+    throw "Please enter a project name";
+  }
 } catch (err) {
   console.error(err);
 }
